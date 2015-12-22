68c89c09baf1e13bb172c0c5eb6f85bab861eae7
kjakubik <krzysiek.jakubik@gmail.com>
2015-12-22 11:23:05 +0100

    Split infrastructure configuration into stages

--- infrastructure/test.js
+++ infrastructure/test.js
@@ -1,7 +1,11 @@
 var _ = require('lodash');
+var heroin = require('heroin-js');
+var configurator = heroin(process.env.HEROKU_API_TOKEN, {debug: false});
 
-module.exports = _.merge({}, require('./shared.js'), {
+var config = _.merge({}, require('./shared.js'), {
     name: 'book-inventory-s-test',
     domains: [
     ]
 });
+
+configurator(config);
