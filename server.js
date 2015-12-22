32d8965945fb6d4a8c55f25dae13c4284e4b2bb5
kjakubik <krzysiek.jakubik@gmail.com>
2015-12-22 09:46:34 +0100

    Use environment variables as configuration

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var app = require('./app')();
 
-var server = app.listen(5000, function() {
-
+var server = app.listen(process.env.PORT || 5000, function() {
+    console.log("Express server listening on port %d", server.address().port);
 });
--- storage/mongo.js
+++ storage/mongo.js
@@ -1,6 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
 
-var dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/book_inventory_service';
+var dbUrl = process.env.DB_URL || process.env.MONGOLAB_URI || 'mongodb://localhost:27017/book_inventory_service';
 var connection = MongoClient.connect(dbUrl);
 
 module.exports = connection;
